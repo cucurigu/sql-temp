83947d36bbf26639d846031703479bbab1958265
greg <greg.len@outlook.com>
2018-08-30 12:51:20 +0100

    WIP

--- subqueries/final_submit.sql
+++ subqueries/final_submit.sql
@@ -7,6 +7,11 @@ WHERE sm.ExamMark > (SELECT AVG(ExamMark) FROM dbo.StudentModule)
 
 -- 2. Show the lecturer(s) with the highest exam weighting for a module. -- ( To be revised )
 
+SELECT m.ModuleConvenor, m.ModuleID, m.ExamWeight AS HighestExamWeight
+FROM dbo.Module m
+where M.ExamWeight = (SELECT MAX(m2.ExamWeight)
+                      FROM dbo.Module AS m2
+                      WHERE m.ModuleConvenor = m2.ModuleConvenor)
 
 -- 3. Select all courses where an ‘A’ has been awarded, showing course code and description. Do this using both a (a) nested query and (b) joins. --
 
@@ -39,6 +44,13 @@ WHERE TheRank = 1
 
 -- 6. Display the ID, Name and average Exam and Coursework weights for all lecturers and make sure all the columns have suitable headings, using a correlated query. Investigate use of the ISNULL function to produce suitable output in the case of a lecturer not teaching any modules --
 
+SELECT l.StaffID,
+  CONCAT(l.FirstName, ' ', l.Surname) AS FullName,
+  ISNULL(CAST((SELECT AVG(m2.ExamWeight) FROM dbo.Module AS m2 WHERE m.ModuleConvenor = m2.ModuleConvenor GROUP BY ModuleConvenor) AS NVARCHAR), 'N/A') AS AvgExamWeight,
+  ISNULL(CAST((SELECT AVG(m2.CWWeight) FROM dbo.Module AS m2 WHERE m.ModuleConvenor = m2.ModuleConvenor GROUP BY ModuleConvenor) AS NVARCHAR), 'N/A') AS AvgCWWeight
+FROM dbo.Module AS m
+  FULL JOIN dbo.Lecturer l ON m.ModuleConvenor = l.StaffID
+GROUP BY l.StaffID, l.Surname, l.FirstName, m.ModuleConvenor
 
 -- 7. Select all courses that include both modules CS2026 and BS2029, showing their ID and description. --
 
@@ -50,6 +62,16 @@ HAVING Count(*) = 2)
 -- 8. Show all the students that have completed both BS and CS courses, displaying their ID’s, names and the date completed. Investigate the use of CONVERT to display the date completed in such a way that it only shows the date (UK style). -- 
 
 -- (Only works if the dates are the same ?)
+
+
+SELECT sc.StudentID, s.FirstName, s.Surname, CONVERT(varchar, CONVERT(datetime, sc.DateCompleted) , 103) AS DateCompleted
+FROM dbo.StudentCourse sc
+JOIN dbo.Student s ON sc.StudentID = s.StudentID
+WHERE sc.CourseCode IN (SELECT c.CourseCode FROM dbo.Course c WHERE c.CourseCode IN ('BS','CS')) AND sc.StudentID IN (SELECT StudentID FROM dbo.StudentCourse
+GROUP BY StudentID
+HAVING SUM(1) > 1)
+GROUP BY sc.StudentID, s.FirstName, s.Surname, sc.DateCompleted
+
 
 SELECT sc.StudentID, s.FirstName
 	, s.Surname
