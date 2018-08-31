2bd033a22d0500941256a4f2d3ca085174d35f8d
Martin Swiderski <martinswiderski@googlemail.com>
2018-08-31 16:57:02 +0100

    trigger breaks on Borrower with no type

--- error-handling/view_Trigger.sql
+++ error-handling/view_Trigger.sql
@@ -5,6 +5,9 @@ ALTER TRIGGER trOrg ON dbo.vwOrganisationalBorrower
 INSTEAD OF INSERT
 AS
 BEGIN
+  DECLARE @wasFound INT;
+  SET @wasFound = 2;
+
   IF (SELECT COUNT(*) FROM Inserted) > 0 BEGIN
     IF NOT EXISTS (SELECT 'True' FROM Inserted AS i
                 JOIN dbo.Borrower AS b
@@ -16,26 +19,35 @@ BEGIN
                 JOIN dbo.AcademicBorrower ab
                 ON i.BorrowerID = ab.BorrowerID)
       BEGIN
-      RAISERROR('Not found in Academic Borrower table.', 1, 2)
+        RAISERROR('Not found in Academic Borrower table.', 1, 2)
+        SET @wasFound = @wasFound - 1;
       END
 
       IF NOT EXISTS (SELECT 'True' FROM Inserted AS i
                 JOIN dbo.BusinessBorrower bb
                 ON i.BorrowerID = bb.BorrowerID)
       BEGIN
-      RAISERROR('Not found in Business Borrower table.', 1, 3)
+        RAISERROR('Not found in Business Borrower table.', 1, 3)
+        SET @wasFound = @wasFound - 1;
       END
 
       BEGIN
 
-      INSERT INTO dbo.OrganisationalBorrower(BorrowerID, AccountsDeptAddress)
-        SELECT i.BorrowerID, ' '
-        FROM Inserted AS i
-        JOIN dbo.Borrower b on i.BorrowerID = b.BorrowerID
+        -- if not found in any of the tables and equals zero do not proceed
+        IF (@wasFound = 0) BEGIN
+          RAISERROR('Not found in both Business and Academic Borrower tables. Cannot proceed.', 1, 3)
+          RETURN;
+        END
 
-      INSERT INTO dbo.OrgContact(OrgContactID, BorrowerID, ContactType, ContactDetails)
-        SELECT ' ', i.BorrowerID, 'E', ' '
-        FROM Inserted as i
-    END
+        INSERT INTO dbo.OrganisationalBorrower(BorrowerID, AccountsDeptAddress)
+          SELECT i.BorrowerID, ' '
+          FROM Inserted AS i
+          JOIN dbo.Borrower b on i.BorrowerID = b.BorrowerID
+
+        INSERT INTO dbo.OrgContact(OrgContactID, BorrowerID, ContactType, ContactDetails)
+          SELECT ' ', i.BorrowerID, 'E', ' '
+          FROM Inserted as i
+
+      END
   END
 END
